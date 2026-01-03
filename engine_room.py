74975688d7de8341082e6f8c65788c4ecf1922a8
Sanket-07 <sanketwcth2@gmail.com>
2026-01-03 12:11:38 +0530

    operations on strings and numbers

---
+++ engine_room.py
@@ -0,0 +1,75 @@
+#print hello world
+Msg = "Hello world!"
+print(Msg)
+
+#checking datatype of MSg
+print(type(Msg))
+
+#checking data type of nums
+numOne = 3
+numTwo = 3.0
+print(type(numOne))
+print(type(numTwo))
+
+#checking datatype of False
+val = False
+print(type(val))
+
+#checking nums and false datatypes
+print(numOne)
+print(numTwo)
+print(val)
+
+#prinitng nums and thier datatypes
+num1 = 3
+num2 = 5
+print(num1, num2)
+print(type(num1), type(num2))
+
+#performing add operation
+add = num1 + num2
+print(add)
+
+#performing other operations
+print(num1-num2)
+print(num1*num2)
+print(num2 / num1)
+print(num2 // num1)
+print(num2 % num1)
+print(num2 // num1)
+print(num2 % num1)
+print(3 ** 2)
+
+#checking dataype if we divide
+print(type(num2/num1))
+
+#arithmetic operations can be applied to floats also just like ints
+new1 = 2.1
+new2 = 0.3
+
+print(new1 + new2)
+print(new1 - new2)
+
+#int and float can be added
+solid = 3
+soft = 0.1
+print (solid + soft)
+
+#BODMAS
+print(5 * 4 + 1)
+print(1 + 5 * 4)
+print(5 * (4 + 1) )
+
+#comparism
+print(2 == 2)
+print(2 == 1)
+
+print(3 != 2)
+ 
+print(3 > 2)
+print(3 < 2)
+ 
+print(3 >= 3)
+print(3 >= 2)
+ 
+print(3 <= 2)
